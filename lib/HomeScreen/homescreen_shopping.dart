3bbc6907a4b10d61dbb39dfaf5b35443740b75fb
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-06-20 23:52:43 +0500

    update

--- lib/HomeScreen/homescreen_shopping.dart
+++ lib/HomeScreen/homescreen_shopping.dart
@@ -11,10 +11,49 @@ class _HomeScreen1State extends State<HomeScreen1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Expanded(
-        flex: 1,
-          child:
-          Image.asset('assets/shop.png',fit: BoxFit.contain,)),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/shop.png'), // apni image ka path
+                fit: BoxFit.cover, // cover se image perfect fit hoti hai
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Expanded(child: Center(
+            child: Column(
+              children: [
+                Text('Online Shopping\n Sales Product',style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400
+                ),),
+                SizedBox(height: 10,),
+                TextButton(onPressed: (){}, child: Container(
+                  height: 48,
+                  width: 219,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(width: 70,),
+                      Text('Lets Go',style: TextStyle(color: Colors.white),),
+                        SizedBox(width: 32,),
+                        Icon(Icons.arrow_forward,color: Colors.white,)
+                    ],
+                  ),
+                ))
+              ],
+            ),
+          ))
+            ]
+      )
      );
   }
 }

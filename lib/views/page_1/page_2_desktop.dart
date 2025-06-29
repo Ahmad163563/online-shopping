86d9c3b00c620a923cc8c1fe06195dd8d9158747
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-06-29 20:35:08 +0500

    adiing image

--- lib/views/page_1/page_2_desktop.dart
+++ lib/views/page_1/page_2_desktop.dart
@@ -13,75 +13,93 @@ class _Page1screenState extends State<Page1screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children:[
-          SizedBox(height: 30,),
-          Row(
-            children: [
-              SizedBox(width: 100,),
-              Icon(Icons.merge_type_rounded,color: Colors.red,),
-              SizedBox(width: 10,),
-              Text('FORSTR',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
-              SizedBox(width: 180,),
-              Text('HOME',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
-              SizedBox(width: 50,),
-              Text('SERVICE',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
-              SizedBox(width: 50,),
-              Text('PORTFOLIO',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
-              SizedBox(width: 50,),
-              Text('RESUME',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
-              SizedBox(width: 50,),
-              Text('CONTACT',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
-              SizedBox(width: 150,),
-              Container(
-                height: 50,
-                width: 150,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  border: Border.all(),
-                  color: Colors.red
+      body:  SingleChildScrollView(
+        child: Column(
+          children:[
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                SizedBox(width: 100,),
+                Icon(Icons.merge_type_rounded,color: Colors.red,),
+                SizedBox(width: 10,),
+                Text('FORSTR',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                SizedBox(width: 180,),
+                Text('HOME',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
+                SizedBox(width: 50,),
+                Text('SERVICE',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
+                SizedBox(width: 50,),
+                Text('PORTFOLIO',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
+                SizedBox(width: 50,),
+                Text('RESUME',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
+                SizedBox(width: 50,),
+                Text('CONTACT',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white),),
+                SizedBox(width: 150,),
+                Container(
+                  height: 50,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(),
+                    color: Colors.red
+                  ),
+                  child: Center(child: Text('DOWNLOAD CV',style: TextStyle(color: Colors.white,fontWeight: FontWeight.normal),)),
                 ),
-                child: Center(child: Text('DOWNLOAD CV',style: TextStyle(color: Colors.white,fontWeight: FontWeight.normal),)),
-              ),
-
-            ],
-          ),
-          SizedBox(height: 100,),
-          Row(
-            children: [
-              SizedBox(width: 100,),
-              Text('Hello, ',style:  TextStyle(fontWeight: FontWeight.normal,color: Colors.white,fontSize: 50),),
-              SizedBox(width: 7,),
-              Text('I^m',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.red,fontSize: 50),)
-            ],
-          ),
-          SizedBox(height: 10,),
-          Row(
-            children: [
-              SizedBox(width: 100,),
-              Text('Ahmad',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 100),),
-              SizedBox(width: 30,),
-              Text('Yar',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 100))
-            ],
-          ),
-          SizedBox(height: 7,),
-          Row(
-            children: [
-              SizedBox(width: 100,),
-              Text('APP Designer And APP Developer ',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white,fontSize: 50),)
-            ],
-          ),
-          SizedBox(height: 3,),
-          Row(
-            children: [
-              SizedBox(width: 80,),
-              Text('I am a passionate and results-driven Flutter Developer with a strong focus on building high-performance, responsive, \n and visually appealing cross-platform mobile applications.\n With a solid understanding of Dart and Flutter’s widget-based architect'
-                  'ure, I specialize in crafting clean UI designs,\n implementing efficient state management, and o'
-                  'ptimizing performance across Android, web, and deskt'
-                  'op platforms.',style: TextStyle(color: Colors.white,fontSize: 10),)
-            ],
-          )
-    ]
+        
+              ],
+            ),
+            SizedBox(height: 50,),
+            Row(
+              children: [
+                SizedBox(width: 100,),
+                Text('Hello, ',style:  TextStyle(fontWeight: FontWeight.normal,color: Colors.white,fontSize: 50),),
+                SizedBox(width: 7,),
+                Text('I^m',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.red,fontSize: 50),)
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                SizedBox(width: 100,),
+                Text('Ahmad',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 100),),
+                SizedBox(width: 30,),
+                Text('Yar',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 100))
+              ],
+            ),
+            SizedBox(height: 7,),
+            Row(
+              children: [
+                SizedBox(width: 100,),
+                Text('APP Designer And APP Developer ',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white,fontSize: 50),)
+              ],
+            ),
+            SizedBox(height: 3,),
+            Row(
+              children: [
+                SizedBox(width: 95,),
+                Text('I am a passionate and results-driven Flutter Developer with a strong focus on building high-performance, responsive, \n and visually appealing cross-platform mobile applications.\n With a solid understanding of Dart and Flutter’s widget-based architect'
+                    'ure, I specialize in crafting clean UI designs,\n implementing efficient state management, and o'
+                    'ptimizing performance across Android, web, and deskt'
+                    'op platforms.',style: TextStyle(color: Colors.white,fontSize: 20),)
+              ],
+            ),
+              SizedBox(height: 23,),
+              Row(children:[
+                SizedBox(width: 95,),
+                Text(''
+                'FIND ME ON',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)]),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    SizedBox(width: 80,),
+                    Container(height: 20,width: 50,color: Colors.black,child:Image.asset('assets/fb.jpg',color: Colors.black,),),
+                    Container(height: 20,width: 50,color: Colors.black,child:Image.asset('assets/twitter.png',color: Colors.white,),),
+                    Container(height: 20,width: 50,color: Colors.black,child:Image.asset('assets/linkdein.png',color: Colors.white,),),
+                    Container(height: 20,width: 50,color: Colors.black,child:Image.asset('assets/utube.png',color: Colors.white,),)
+                  ],
+                )        
+            
+            ]
+        ),
       ),
       );
 
